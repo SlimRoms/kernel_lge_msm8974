7b76ab5728a055cfa7188211b247267b34712360
Robbie Lancaster <RobbieLancaster811@gmail.com>
2014-07-26 13:56:58 +0200

    Add back Slim governor
    thanks a ton to flar2
    
    Change-Id: If2a6afa5919894234472ce56197ca58ca92fb8d8

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -45,6 +45,10 @@
 #define INIT_UDELAY		200
 #define MAX_UDELAY		2000
 
+#ifdef CONFIG_CPU_FREQ_GOV_SLIM
+int graphics_boost = 6;
+#endif
+
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -173,8 +177,12 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	 * don't do this then the pwrlevel change won't take effect when the
 	 * clocks come back
 	 */
+    pwr->active_pwrlevel = new_level;
 
-	pwr->active_pwrlevel = new_level;
+#ifdef CONFIG_CPU_FREQ_GOV_SLIM
+        graphics_boost = pwr->active_pwrlevel;
+#endif
+
 	pwr->bus_mod = 0;
 	pwrlevel = &pwr->pwrlevels[pwr->active_pwrlevel];
 
